044ed83a4c882cf6b0dd0c1789a7e5eb08e8f3bd
Unique-Ikeoluwa <ikeoluwaadegbite@gmail.com>
2025-03-10 02:22:00 +0100

    finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
  export default {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        btn: '#ffac00',
+        body: '#fafafa',
+        bg1: '#212121',
+        bgc: '#a15842',
+        bgc1: '#151515',
+        fgc: '#7a8579'
+      },
+      space: ({ theme }) => ({
+        ...theme('spacing'),
+      }),
+      spacing: {
+        bpx: '120px',
+        mpx: '690px'
+     },
+     minWidth: ({ theme }) => ({
+      ...theme('spacing')
+     }),
+     borderRadius: {
+      bro: '40px'
+     }
+    }
   },
   plugins: [],
 }
